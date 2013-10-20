d1827859dbee554649579396da271f0e403cc443
David Tang <dtangster@yahoo.com>
2013-10-20 14:37:14 -0700

    Updated SymbolTableEntry

--- src/intermediate/SymbolTableEntry.java
+++ src/intermediate/SymbolTableEntry.java
@@ -5,8 +5,26 @@ import java.util.HashMap;
 // We are not using the HashMap it extends for this assignment.
 public class SymbolTableEntry extends HashMap<Attribute, Object> {
     private String name;
+    private SymbolTable symbolTable; // The table that contains this entry
 
-    public SymbolTableEntry() {
+    public SymbolTableEntry(String name, SymbolTable symbolTable) {
+        this.name = name;
+        this.symbolTable = symbolTable;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public SymbolTable getSymbolTable() {
+        return symbolTable;
+    }
+
+    public void setAttribute(Attribute key, Object value) {
+        put(key, value);
+    }
+
+    public Object getAttribute(Attribute key) {
+        return get(key);
     }
 }
