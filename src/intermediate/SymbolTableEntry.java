dcd40c2b4bae2d3e75418491eca4e164b290c90d
David Tang <dtangster@yahoo.com>
2013-10-19 21:13:54 -0700

    Removed ParseTree and added nodes to SymbolTableEntry

--- src/intermediate/SymbolTableEntry.java
+++ src/intermediate/SymbolTableEntry.java
@@ -5,4 +5,10 @@ import java.util.HashMap;
 public class SymbolTableEntry {
     private String name;
     private HashMap<Attribute, Object> attributes;
+    private SymbolTableEntry car;
+    private SymbolTableEntry cdr;
+
+    public SymbolTableEntry() {
+
+    }
 }
