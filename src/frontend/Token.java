ce197ad0f348a58f354242340be14916e3ae88e4
David Tang <dtangster@yahoo.com>
2013-10-21 18:12:42 -0700

    Removed some comments to make things easier to read.

--- src/frontend/Token.java
+++ src/frontend/Token.java
@@ -10,11 +10,6 @@ public class Token {
     protected int lineNum;     // line number of the token's source line
     protected int position;    // position of the first token character
 
-    /**
-     * Constructor.
-     * @param source the source from where to fetch the token's characters.
-     * @throws Exception if an error occurred.
-     */
     public Token(Source source) throws IOException {
         this.source = source;
         this.lineNum = source.getLineNum();
@@ -22,86 +17,39 @@ public class Token {
         extract();
     }
 
-    /**
-     * Getter
-     * @return the token type
-     */
-    public TokenType getType()
-    {
+    public TokenType getType() {
         return type;
     }
 
-    /**
-     * Getter.
-     * @return the token text.
-     */
-    public String getText()
-    {
+    public String getText() {
         return text;
     }
 
-    /**
-     * Getter.
-     * @return the token value.
-     */
-    public Object getValue()
-    {
+    public Object getValue() {
         return value;
     }
 
-    /**
-     * Getter.
-     * @return the source line number.
-     */
-    public int getLineNumber()
-    {
+    public int getLineNumber() {
         return lineNum;
     }
 
-    /**
-     * Getter.
-     * @return the position.
-     */
-    public int getPosition()
-    {
+    public int getPosition() {
         return position;
     }
 
-    /**
-     * Default method to extract only one-character tokens from the source.
-     * Subclasses can override this method to construct language-specific
-     * tokens.  After extracting the token, the current source line position
-     * will be one beyond the last token character.
-     * @throws Exception if an error occurred.
-     */
     protected void extract() throws IOException {
         text = Character.toString(currentChar());
         nextChar();  // consume current character
     }
 
-    /**
-     * Call the source's currentChar() method.
-     * @return the current character from the source.
-     * @throws Exception if an error occurred.
-     */
     protected char currentChar() throws IOException {
         return source.currentChar();
     }
 
-    /**
-     * Call the source's nextChar() method.
-     * @return the next character from the source after moving forward.
-     * @throws Exception if an error occurred.
-     */
     protected char nextChar() throws IOException {
         return source.nextChar();
     }
 
-    /**
-     * Call the source's peekChar() method.
-     * @return the next character from the source without moving forward.
-     * @throws Exception if an error occurred.
-     */
     protected char peekChar() throws IOException {
         return source.peekChar();
     }
