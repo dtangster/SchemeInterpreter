fe6e557fadff0ece7a26cc0552a363aba7a2d1f6
David Tang <dtangster@yahoo.com>
2013-10-28 17:54:02 -0700

    Fixed more printing.

--- src/frontend/tokens/WordToken.java
+++ src/frontend/tokens/WordToken.java
@@ -30,10 +30,12 @@ public class WordToken extends Token {
         if (text.equals("let") && currentChar() == '*') {
             text = "let*";
             type = TokenType.ALL_SYMBOLS.get(text);
+            nextChar();
         }
         else if (text.equals("null") && currentChar() == '?') {
             text = "null?";
             type = TokenType.ALL_SYMBOLS.get(text);
+            nextChar();
         }
         else if (type == null) {
             type = TokenType.REGULAR_SYMBOL;
