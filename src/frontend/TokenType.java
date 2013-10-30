eb05355616db2a00cdd11683f2f65c21c1873d1f
David Tang <dtangster@yahoo.com>
2013-10-29 17:32:58 -0700

    More changes to printing

--- src/frontend/TokenType.java
+++ src/frontend/TokenType.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 
 public enum TokenType {
     // Reserved words (Does not go into symbol table)
-    AND, COND, DEFINE, ELSE, IF, LAMBDA, LET, LETREC, LETSTAR("let*"), NOT, OR, CONS,
+    AND, COND, CONS, DEFINE, ELSE, IF, LAMBDA, LET, LETREC, LETSTAR("let*"),
+    MAP, NOT, NULL("null?"), OR,
 
     // Reserved special symbols (Does not go into symbol table)
-    QUOTE("'"), LEFT_PAREN("("), RIGHT_PAREN(")"),
+    LEFT_PAREN("("), RIGHT_PAREN(")"), QUOTE("'"),
 
     // Others (Does not go into symbol table)
     INTEGER, REAL, STRING, RESERVED_SYMBOL, REGULAR_SYMBOL,
@@ -16,10 +17,10 @@ public enum TokenType {
     ERROR, END_OF_FILE;
 
     private static final int FIRST_RESERVED_WORD_INDEX = AND.ordinal();
-    private static final int LAST_RESERVED_WORD_INDEX = CONS.ordinal();
+    private static final int LAST_RESERVED_WORD_INDEX = OR.ordinal();
 
-    private static final int FIRST_RESERVED_SYMBOL_INDEX = QUOTE.ordinal();
-    private static final int LAST_RESERVED_SYMBOL_INDEX  = RIGHT_PAREN.ordinal();
+    private static final int FIRST_RESERVED_SYMBOL_INDEX = LEFT_PAREN.ordinal();
+    private static final int LAST_RESERVED_SYMBOL_INDEX  = QUOTE.ordinal();
 
     private String text;  // token text
 
